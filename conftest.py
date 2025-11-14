edba09bfb27c4b5abd04b79159b741cf0e1ccfc9
Pierre-Louis Pagliero <pierrelouis.pagliero@gmail.com>
2025-11-14 01:09:58 +0100

    Fix tests

--- conftest.py
+++ conftest.py
@@ -1,9 +1,11 @@
-"""Configuration for Voltalis integration tests."""
+"""Top-level test configuration for Voltalis integration."""
 
 from collections.abc import Generator
 from typing import Any
 
 import pytest
+
+pytest_plugins = ["pytest_homeassistant_custom_component"]
 
 
 @pytest.fixture(autouse=True)
