1821ca00ebc5034d6689ec6609b914c112687ed9
Pierre-Louis Pagliero <pierrelouis.pagliero@gmail.com>
2026-06-01 12:50:34 +0200

    Fix typing

--- custom_components/voltalis/lib/infrastructure/providers/voltalis_client_stub.py
+++ custom_components/voltalis/lib/infrastructure/providers/voltalis_client_stub.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, cast
 
 from custom_components.voltalis.lib.domain.shared.exceptions import VoltalisAuthenticationException
 from custom_components.voltalis.lib.domain.shared.providers.http_client import (
@@ -49,7 +49,7 @@ class VoltalisClientStub(HttpClient):
                 method="POST",
                 body=payload,
             )
-            return response.data["token"]
+            return cast(str, response.data["token"])
         except HttpClientException as err:
             if err.response and err.response.status == 401:
                 raise VoltalisAuthenticationException("Invalid username or password") from err
